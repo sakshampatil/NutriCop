ff4980bf50326cce4557dc2413a2623863984eb3
Saksham Patil <sakshampatil815@gmail.com>
2023-12-04 09:46:18 +0530

    ts server initialized

---
+++ backend/server.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from "express";
+import mysql, { Connection } from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+const db: Connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234@Abcd",
+  database: "calories_tracker",
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.json("Hey Saksham It Works");
+});
+
+app.get("/rawItems", (req: Request, res: Response) => {
+  const q = "SELECT * FROM raw_items";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    else return res.json(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is up and running");
+});
