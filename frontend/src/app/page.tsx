85c6fb9075fffe194a6e487ea6e90fea7becac7e
Saksham Patil <sakshampatil815@gmail.com>
2024-03-14 00:38:03 +0530

    next auth intialized

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,23 @@
 "use client";
+//From React and Nextjs
 import { Fragment } from "react";
 
+//Custom made components
 import Navbar from "@/components/navbar/Navbar";
 
+//Third Party packages
 import { NextUIProvider } from "@nextui-org/system";
+import { SessionProvider } from "next-auth/react";
 import LandingPage from "./landing/page";
 
 export default function Home() {
   return (
     <Fragment>
       <NextUIProvider>
-        <Navbar />
-        <LandingPage />
+        <SessionProvider>
+          <Navbar />
+          <LandingPage />
+        </SessionProvider>
       </NextUIProvider>
     </Fragment>
   );
--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: FC = () => {
     <div className="flex justify-between items-center bg-neutral-950 py-2 px-3 absolute top-0 right-0 left-0">
       <div className="text-white font-bold text-lg">NutriCop</div>
       <div className="">
-        <Button radius="full" className="text-white bg-blue-600 px-3 py-2 rounded-lg ">
+        <Button radius="full" className="text-white bg-blue-600 px-3 py-2 rounded-lg font-normal">
           Login
         </Button>
       </div>
