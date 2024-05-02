c49aa1f484c50ee4f154d350fcb69a71d95de992
Saksham Patil <sakshampatil815@gmail.com>
2024-05-02 23:55:26 +0530

    added route path to middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,3 +1,13 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard", "/ingredients"] };
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/ingredients",
+    "/ingredients/addIngredients",
+    "/recipes",
+    "/recipes/addRecipes",
+    "/meals",
+    "/days",
+  ],
+};
